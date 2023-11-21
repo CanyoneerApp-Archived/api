620f1010368cf3e1cc93c19068ecd12e3700d625
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-21 09:03:11 -0800

    Update README, run prettier

--- src/utils/parseAdditionalRisk.ts
+++ src/utils/parseAdditionalRisk.ts
@@ -1,21 +1,21 @@
-export type AdditionalRisk = 'PG-13' | 'PG' | 'XXX' | 'XX' | 'X' | 'R'
+export type AdditionalRisk = 'PG-13' | 'PG' | 'XXX' | 'XX' | 'X' | 'R';
 
 const lookups: [string | RegExp, AdditionalRisk][] = [
-    [/\bPG-13\b/, 'PG-13'],
-    [/\bPG\b/, 'PG'],
-    [/\bXXX\b/, 'XXX'],
-    [/\bXX\b/, 'XX'],
-    [/\bX\b/, 'X'],
-    [/\bR\b/, 'R'],
+  [/\bPG-13\b/, 'PG-13'],
+  [/\bPG\b/, 'PG'],
+  [/\bXXX\b/, 'XXX'],
+  [/\bXX\b/, 'XX'],
+  [/\bX\b/, 'X'],
+  [/\bR\b/, 'R'],
 ];
 
 export default function parseAdditionalRisk(input: string | undefined): AdditionalRisk | undefined {
-    input = input ? input : '';
+  input = input ? input : '';
 
-    for (const lookup of lookups) {
-        const [key, value] = lookup;
-        if (input.match(key)) {
-            return value;
-        }
+  for (const lookup of lookups) {
+    const [key, value] = lookup;
+    if (input.match(key)) {
+      return value;
     }
+  }
 }
--- src/utils/parseTable.ts
+++ src/utils/parseTable.ts
@@ -1,27 +1,35 @@
 import _, {toArray} from 'lodash';
 
 export function parseTable(tableEl: Element | null | undefined) {
-    if (!tableEl) return {};
+  if (!tableEl) return {};
 
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const one = _.fromPairs(toArray(tableEl!.children).map(tr => {
-        const th = tr.querySelector('th');
-        if (!th) {return [];} else {return [th.textContent?.slice(0, th.textContent.length - 1), tr.querySelector('td')];}
-    }));
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const one = _.fromPairs(
+    toArray(tableEl!.children).map(tr => {
+      const th = tr.querySelector('th');
+      if (!th) {
+        return [];
+      } else {
+        return [th.textContent?.slice(0, th.textContent.length - 1), tr.querySelector('td')];
+      }
+    }),
+  );
 
-    const two = _.fromPairs(toArray(tableEl.querySelectorAll('div.detailsRow')).map(tr => {
-        const th = tr.querySelector('.detailsRowDescriptor');
-        if (!th) {
-            return [];
-        } else {
-            // permits has a <u> instead of a <span>
-            let found = _.last(tr.querySelectorAll('u'))
-            if (found == null) {
-                found = _.last(tr.querySelectorAll('span'))
-            }
-            return [th.textContent?.slice(0, th.textContent.length - 1), found];
+  const two = _.fromPairs(
+    toArray(tableEl.querySelectorAll('div.detailsRow')).map(tr => {
+      const th = tr.querySelector('.detailsRowDescriptor');
+      if (!th) {
+        return [];
+      } else {
+        // permits has a <u> instead of a <span>
+        let found = _.last(tr.querySelectorAll('u'));
+        if (found == null) {
+          found = _.last(tr.querySelectorAll('span'));
         }
-    }));
+        return [th.textContent?.slice(0, th.textContent.length - 1), found];
+      }
+    }),
+  );
 
-    return {...one, ...two};
+  return {...one, ...two};
 }
