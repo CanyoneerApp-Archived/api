b5045859d9afe8520191110694c96a86102868ef
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-23 12:55:49 -0800

    Proposal for new type interface

--- src/scrape/Route.ts
+++ src/scrape/Route.ts
@@ -5,7 +5,50 @@ import {Month} from './parseMonths';
 import {Sport} from './parseSports';
 import {Time} from './parseTime';
 
-export interface Route {
+export interface RouteIndex {
+  id: string;
+  name: string;
+  stars: number;
+  months: Month[];
+  technicalGrade: TechnicalGrade | undefined;
+  waterGrade: WaterGrade | undefined;
+  timeGrade: TimeGrade | undefined;
+  additionalRisk: AdditionalRisk | undefined;
+  permits: Permit | undefined;
+  rappelCountMin: number | undefined;
+  rappelCountMax: number | undefined;
+  rappelLengthMax: number | undefined;
+  detailsUrl: string;
+  vehicle: Vehicle | undefined;
+  shuttle: Shuttle | undefined;
+}
+
+export interface RouteDetails extends RouteIndex {
+  description: string;
+  geojson: {type: 'FeatureCollection', features: RouteFeature[]};
+  url: string;
+}
+
+export interface RouteFeatureProperties {
+  name: string;
+  description: string;
+}
+
+export type RouteFeaturePropertiesLineString = {
+  type: 'approach' | 'exit' | 'shuttle' | 'descent'
+} & {[Key in keyof RouteIndex as `route.${Key}`]: RouteIndex[Key]}
+
+export type RouteFeatureLineString = Feature<LineString, RouteFeaturePropertiesLineString>
+
+export type RouteFeaturePropertiesProperties = {
+  type: 'waypoint'
+} & {[Key in keyof RouteIndex as `route.${Key}`]: RouteIndex[Key]}
+
+export type RouteFeaturePoint = Feature<Point, RouteFeaturePropertiesProperties>
+
+export type RouteFeature = RouteFeature[]
+
+export interface LegacyRoute {
   URL: string;
   Name: string;
   Quality: number;
