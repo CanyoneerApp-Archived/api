2d0a8e099171b55891888e9344059cff0fc4ef31
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-24 08:12:46 -0800

    Add tippecanoe docs

--- src/writeTippecanoe.ts
+++ src/writeTippecanoe.ts
@@ -4,12 +4,21 @@ import tippecanoe from 'tippecanoe';
 export async function writeTippecanoe() {
   await tippecanoe(
     ['./output/index.geojson'],
+
+    // You can pass any option from https://github.com/mapbox/tippecanoe. The node wrapper will
+    // convert keys from camelCase to kebab-case.
     {
-      // TODO add line simplification
       outputToDirectory: './output/tiles',
-      zg: true,
+      maximumZoom: 12,
+
+      // If the tiles are too big at low zoom levels, drop the least- visible features to allow
+      // tiles to be created with those features that remain
       dropDensestAsNeeded: true,
-      extendZoomsIfStillDropping: true,
+
+      // Multiply the tolerance for line and polygon simplification by scale.The standard tolerance
+      // tries to keep the line or polygon within one tile unit of its proper location.You can
+      // probably go up to about 10 without too much visible difference.
+      simplification: 10,
     },
     {echo: true},
   );
