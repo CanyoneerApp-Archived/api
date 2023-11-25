6751f1489a0ef9497ebece5947c385bf05303001
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-24 16:11:00 -0800

    wip

--- src/scrape/scrapeDescriptions.ts
+++ src/scrape/scrapeDescriptions.ts
@@ -2,47 +2,43 @@ import {chunk as lodashChunk} from 'lodash';
 import {parseString} from 'xml2js';
 import {IndexRoute} from '../Route';
 
+type IndexRouteWithDescription = IndexRoute & {description: string};
+
 export async function scrapeDescriptions(
-  indicies: IndexRoute[],
-): Promise<(IndexRoute & {description: string})[]> {
-  const chunks = lodashChunk(indicies, 50);
-  const output: (IndexRoute & {description: string})[] = [];
+  routes: IndexRoute[],
+): Promise<IndexRouteWithDescription[]> {
+  const routeChunks = lodashChunk(routes, 50);
 
-  for (const chunk of chunks) {
-    // TODO there's actually a ton of meatadata in here, do we even need the first pass over the index data?
-    const url = `http://ropewiki.com/api.php?format=json&action=query&export=true&pageids=${chunk
-      .map(index => index.id)
-      .join('|')}`;
+  return (
+    await Promise.all(
+      routeChunks.map(async routeChunk => {
+        // TODO make this a URL
+        // TODO find docs for this API
+        // TODO there's actually a ton of metadata in here, do we even need the first pass over the index data?
+        const url = `http://ropewiki.com/api.php?format=json&action=query&export=true&pageids=${routeChunk
+          .map(index => index.id)
+          .join('|')}`;
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const xml: any = await new Promise(async (resolve, reject) =>
-      parseString((await (await fetch(url)).json()).query.export['*'], (err, result) => {
-        if (err) reject(err);
-        else resolve(result);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const xml: any = await new Promise(async (resolve, reject) =>
+          parseString((await (await fetch(url)).json()).query.export['*'], (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          }),
+        );
+
+        return routeChunk.map((index): IndexRouteWithDescription => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const text = xml.mediawiki.page.find((page: any) => page.id[0] === index.id).revision[0]
+            .text[0]._;
+
+          return {
+            ...index,
+            // TODO render this as HTML
+            description: text.slice(text.indexOf('==Introduction==')),
+          };
+        });
       }),
-    );
-
-    for (let i = 0; i < chunk.length; i++) {
-      const index = chunk[i];
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const text = xml.mediawiki.page.find((page: any) => page.id[0] === index.id).revision[0]
-        .text[0]._;
-
-      const startIndex = text.indexOf('==Introduction==');
-
-      const o = {
-        ...index,
-        // TODO render this as HTML
-        description: text.slice(startIndex),
-      };
-
-      console.log(o);
-
-      output.push(o);
-    }
-  }
-
-  // @ts-ignore
-  return output;
+    )
+  ).flat();
 }
