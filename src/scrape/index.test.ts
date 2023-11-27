d76cfcde26d407698d08bdeaa6d68e69c804dbd1
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-26 17:28:38 -0800

    Add test for v1 conversion

--- src/scrape/index.test.ts
+++ src/scrape/index.test.ts
@@ -1,4 +1,5 @@
 import {inner as scrapeInner} from '.';
+import {toRouteV1} from '../types/RouteV1';
 import {writeAllSchemas} from '../writeAllSchemas';
 
 describe('scrape', () => {
@@ -10,6 +11,15 @@ describe('scrape', () => {
     'Maine',
     async () => {
       return expect(await scrapeInner(['Maine'])).toMatchSnapshot();
+    },
+    60 * 1000,
+  );
+
+  it(
+    'Maine v1',
+    async () => {
+      const routes = await scrapeInner(['Maine']);
+      return expect(routes.map(toRouteV1)).toMatchSnapshot();
     },
     60 * 1000,
   );
