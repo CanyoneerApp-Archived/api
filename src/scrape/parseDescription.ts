7182f0cfa9d5fa9a22ec6afa0d320e7f0903a09c
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-28 19:26:43 -0800

    Fix pandoc crashes and timeouts (#43)
    I ran into two problems when running Pandoc on the entire corpus of
    RopeWiki routes:
    
    1. Starting too many instances of `pandoc` at once (it's a separate
    program) would cause the system to run out of resources resulting in an
    `EAGAIN` crash. I fixed this by throttling the number of calls per
    second.
    2. Some route descriptions caused `pandoc` to crash resulting in a
    program hang. I added code to fail gracefully in this case and created
    #32 for further investigation.

---
+++ src/scrape/parseDescription.ts
@@ -0,0 +1,45 @@
+// @ts-ignore
+import pandoc from 'node-pandoc';
+import PromiseThrottle from 'promise-throttle';
+import {sleep} from './sleep';
+
+/**
+ * Limit the number of Pandoc processes created to 500 per second to prevent the system from running
+ * out of file descriptors.
+ */
+export const promiseThrottle = new PromiseThrottle({requestsPerSecond: 500});
+
+/**
+ * If Pandoc takes longer than this to run, throw a `PandocTimeoutError`. Unexpected input can cause
+ * Pandoc to hang indefinitely.
+ */
+const timeoutMilliseconds = 5000;
+
+/**
+ * Turn MediaWiki markup into HTML
+ */
+export async function parseDescription(input: string): Promise<string> {
+  return promiseThrottle.add(async () => {
+    const timeout = sleep(timeoutMilliseconds);
+
+    return await Promise.race([
+      timeout.then(() => {
+        throw new PandocTimeoutError();
+      }),
+
+      new Promise<string>((resolve, reject) =>
+        pandoc(
+          input.slice(input.indexOf('==Introduction==')),
+          '-f mediawiki -t html',
+          (error: Error, html: string) => {
+            timeout.cancel();
+            if (error) reject(error);
+            else resolve(html);
+          },
+        ),
+      ),
+    ]);
+  });
+}
+
+export class PandocTimeoutError extends Error {}
--- src/scrape/scrapeDescriptions.ts
+++ src/scrape/scrapeDescriptions.ts
@@ -1,10 +1,10 @@
 import {chunk as lodashChunk} from 'lodash';
 // @ts-ignore
-import pandoc from 'node-pandoc';
 import XML2JS from 'xml2js';
 import {IndexRouteV2, RouteV2} from '../types/RouteV2';
 import cachedFetch from './cachedFetch';
 import {validate} from './getValidator';
+import {parseDescription} from './parseDescription';
 
 /**
  * Take an array of `RouteV2`s, scrape their KMLs, and return a new array of routes with the
@@ -55,20 +55,4 @@ export async function scrapeDescriptions(routes: IndexRouteV2[]): Promise<RouteV
       }),
     )
   ).flat();
-}
-
-/**
- * Turn MediaWiki markup into HTML
- */
-function parseDescription(input: string) {
-  return new Promise<string>((resolve, reject) =>
-    pandoc(
-      input.slice(input.indexOf('==Introduction==')),
-      '-f mediawiki -t html',
-      (error: Error, result: string) => {
-        if (error) reject(error);
-        else resolve(result);
-      },
-    ),
-  );
 }
