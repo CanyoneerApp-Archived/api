7d0a1094a21a3873a00a53ff6f96df436f42961d
Lucas Wojciechowski <hello@lucaswoj.com>
2023-11-28 19:24:47 -0800

    Add more comments to parseDescription

--- src/scrape/parseDescription.ts
+++ src/scrape/parseDescription.ts
@@ -3,18 +3,27 @@ import pandoc from 'node-pandoc';
 import PromiseThrottle from 'promise-throttle';
 import {sleep} from './sleep';
 
+/**
+ * Limit the number of Pandoc processes created to 500 per second to prevent the system from running
+ * out of file descriptors.
+ */
 export const promiseThrottle = new PromiseThrottle({requestsPerSecond: 500});
+
+/**
+ * If Pandoc takes longer than this to run, throw a `PandocTimeoutError`. Unexpected input can cause
+ * Pandoc to hang indefinitely.
+ */
+const timeoutMilliseconds = 5000;
 
 /**
  * Turn MediaWiki markup into HTML
  */
 export async function parseDescription(input: string): Promise<string> {
   return promiseThrottle.add(async () => {
-    const sleepPromise = sleep(5000);
+    const timeout = sleep(timeoutMilliseconds);
 
     return await Promise.race([
-      // Throw a `PandocTimeoutError` unless `sleepPromise` is cancelled
-      sleepPromise.then(() => {
+      timeout.then(() => {
         throw new PandocTimeoutError();
       }),
 
@@ -23,7 +32,7 @@ export async function parseDescription(input: string): Promise<string> {
           input.slice(input.indexOf('==Introduction==')),
           '-f mediawiki -t html',
           (error: Error, html: string) => {
-            sleepPromise.cancel();
+            timeout.cancel();
             if (error) reject(error);
             else resolve(html);
           },
