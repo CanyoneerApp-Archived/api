17b427be8af2f873b90a0c0bc269e2f6dadaa9dd
Lucas Wojciechowski <hello@lucaswoj.com>
2023-12-14 10:15:28 -0800

    Improve development experience a little

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -8,45 +8,47 @@ mapboxgl.accessToken =
 export function App() {
   console.log(process.env.PUBLIC_URL, window.location.href);
 
-  return (
-    <Map
-      style={{
-        version: 8,
-        sources: {
-          routes: {
-            type: 'vector',
-            tiles: [
-              `${new URL(
-                process.env.PUBLIC_URL,
-                window.location.href,
-              ).toString()}v2/tiles/{z}/{x}/{y}.pbf`,
-            ],
-          },
+  return <Map style={getMapStyle()} />;
+}
+
+function getMapStyle(): mapboxgl.Style {
+  const publicURL = new URL('http://localhost:8001');
+
+  return {
+    version: 8,
+    terrain: {source: 'mapbox-dem', exaggeration: 2},
+    sources: {
+      routes: {
+        type: 'vector',
+        tiles: [`${publicURL}v2/tiles/{z}/{x}/{y}.pbf`],
+        maxzoom: 12,
+      },
+      'mapbox-dem': {
+        type: 'raster-dem',
+        url: 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        tileSize: 512,
+        maxzoom: 14,
+      },
+    },
+    layers: [
+      {
+        id: 'routes',
+        type: 'line',
+        source: 'routes',
+        'source-layer': 'routes',
+        paint: {
+          'line-color': 'red',
+          'line-width': 2,
         },
-        layers: [
-          {
-            id: 'routes',
-            type: 'line',
-            source: 'routes',
-            'source-layer': 'routes',
-            paint: {
-              'line-color': 'red',
-              'line-width': 2,
-            },
-          },
-        ],
-        // @ts-expect-error we are using a very new style spec
-        imports: [
-          {
-            id: 'basemap',
-            url: 'mapbox://styles/mapbox/standard',
-            config: {
-              lightPreset: 'dusk',
-              showPointOfInterestLabels: false,
-            },
-          },
-        ],
-      }}
-    />
-  );
+      },
+    ],
+    // @ts-expect-error we are using a very new style spec
+    imports: [
+      {
+        id: 'basemap',
+        url: 'mapbox://styles/mapbox/standard',
+        config: {},
+      },
+    ],
+  };
 }
