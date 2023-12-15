5ad32ba11be5f09226d1f01bbacfc0ad9ad4fe17
Lucas Wojciechowski <hello@lucaswoj.com>
2023-12-15 07:13:30 -0800

    Self code review

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -7,7 +7,5 @@ mapboxgl.accessToken =
   'pk.eyJ1Ijoic3BpbmRyaWZ0IiwiYSI6ImNqaDg2bDBsdTBmZG0yd3MwZ2x4ampsdXUifQ.7E19C7BhF9Dfd1gdJiYTEg';
 
 export function App() {
-  console.log(process.env.PUBLIC_URL, window.location.href);
-
   return <Map style={getMapStyle()} />;
 }
