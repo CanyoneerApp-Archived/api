f3fc63a9b7e2a2b2b9fcb317bf97f862ee2175dc
Brice Pollock <brice.pollock@gmail.com>
2024-02-23 20:32:26 -0800

    the actual regex fix for rappel numbers

--- src/cli/scrapeRoutes/parseRappelCount.ts
+++ src/cli/scrapeRoutes/parseRappelCount.ts
@@ -5,7 +5,7 @@ import {parseIntSafe} from '../../utils/parseIntSafe';
  */
 export default function parseRappelCount(input: string | undefined) {
   if (!input) return {rappelCountMin: undefined, rappelCountMax: undefined};
-  const match = input.match(/([0-9+])(-([0-9+]))?r/);
+  const match = input.match(/([0-9+]*)(-([0-9+]*))?r/);
   return {
     rappelCountMin: parseIntSafe(match?.[1]),
     rappelCountMax: parseIntSafe(match?.[3] ?? match?.[1]),
